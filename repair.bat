474041129fe0d55426b45fb265d3b1632fc7b54f
L0garithmic <L0garithmic@users.noreply.github.com>
2020-08-14 00:01:56 -0500

    Delete repair.bat

--- repair.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-	echo working (this may take a minute or two)
-
-for %%f in (calibre\to-fix\*.epub) do (	
-	if not exist "02-fixed" mkdir 02-fixed
-	calibre\ebook-convert "%%f" "02-fixed\%%~nf.epub"  --output-profile tablet --chapter --preserve-cover-aspect-ratio
-)
-	del /s /q calibre\to-fix\*.*
-exit
