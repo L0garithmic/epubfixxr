ba424e42885d36b105c460d729d8c5e5d41ab8b4
L0garithmic <craptotal@gmail.com>
2020-08-14 05:21:18 +0000

    StartingFresh

--- folder-to-epub.bat
+++
@@ -1,4 +0,0 @@
-		for %%f in (folders\) do (	
-			if not exist "03-shrunk" mkdir 03-shrunk
-			calibre\ebook-convert "%%f" "03-shrunk\%%~nf.epub"  --output-profile tablet --chapter --preserve-cover-aspect-ratio
-		)
