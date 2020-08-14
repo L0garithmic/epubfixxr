71b33e622711e57953306d714b13d4ee1c122d28
L0garithmic <L0garithmic@users.noreply.github.com>
2020-08-14 00:02:03 -0500

    Delete 04-pdf-to-epub.bat

--- 04-pdf-to-epub.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-	echo working (this may take a minute or two)
-
-for %%f in (01-input\*.pdf) do (	
-	if not exist "02-converted" mkdir 02-converted
-	calibre\ebook-convert "%%f" "02-converted\%%~nf.epub"  --output-profile tablet --chapter --preserve-cover-aspect-ratio
-)
-exit
