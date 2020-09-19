5c352307c4ac162b197f52f302ba626475f2ac7e
L0garithmic <L0garithmic@users.noreply.github.com>
2020-09-19 08:55:23 -0500

    Update 02-optimize-epub-images.bat

--- 02-optimize-epub-images.bat
+++ 02-optimize-epub-images.bat
@@ -6,10 +6,11 @@
 	::Starts the Loop, any epub files in 01-input
 for %%f in (01-input\*.epub) do (	
 	::Creates missing directories - necessary
-	if not exist "03-shrunk" mkdir 03-shrunk
+	if not exist "04-shrunk" mkdir 04-shrunk
 	if not exist "comp\OEBPS\images" mkdir comp\OEBPS\images
 	if not exist "comp\OPS\images" mkdir comp\OPS\images
 	if not exist "comp\images" mkdir comp\images
+	if not exist "comp\rootimg" mkdir comp\rootimg
 	if not exist "comp\decompress" mkdir comp\decompress
 	if not exist "calibre\to-fix" mkdir calibre\to-fix
 
@@ -28,11 +29,15 @@ for %%f in (01-input\*.epub) do (
 		for %%g in ("%cmpd%\%%~nf\images\*.jpg") do (
 			cjpeg -quality 80 "%%g" > comp/images/%%~nxg
 		)
+		for %%g in ("%cmpd%\%%~nf\index*.jpg") do (
+			cjpeg -quality 80 "%%g" > comp/rootimg/%%~nxg
+		)
 	
 	::Copies the compressed images to the book folders
 	xcopy /s /i /q /y "%cmp%\OEBPS\images" "%cmpd%\%%~nf\OEBPS\images"
 	xcopy /s /i /q /y "%cmp%\OPS\images" "%cmpd%\%%~nf\OPS\images"
 	xcopy /s /i /q /y "%cmp%\images" "%cmpd%\%%~nf\images"
+	xcopy /s /i /q /y "%cmp%\rootimg" "%cmpd%\%%~nf"
 
 	::Compresses the books not a finished output
 	7z.exe a "%cmpd%\%%~nf.epub" "%cmpd%\%%~nf" -mx5
@@ -42,8 +47,8 @@ for %%f in (01-input\*.epub) do (
 
 	
 		for %%f in (calibre\to-fix\*.epub) do (	
-			if not exist "03-shrunk" mkdir 03-shrunk
-			calibre\ebook-convert "%%f" "03-shrunk\%%~nf.epub"  --output-profile tablet --chapter --preserve-cover-aspect-ratio
+			if not exist "04-shrunk" mkdir 04-shrunk
+			calibre\ebook-convert "%%f" "04-shrunk\%%~nf.epub" --output-profile tablet --no-default-epub-cover --no-svg-cover
 		)
 	rmdir /Q /S comp
 	del /s /q calibre\to-fix\*.*
