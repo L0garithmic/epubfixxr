7b8845e11e62e0d76b01981bf1bbcef230374c95
L0garithmic <L0garithmic@users.noreply.github.com>
2020-09-23 10:50:52 -0500

    Update 06-pdf-to-epub.bat

--- 06-pdf-to-epub.bat
+++ 06-pdf-to-epub.bat
@@ -4,34 +4,12 @@
 	=if not exist "calibre\temp" mkdir calibre\temp
 
 for %%f in (01-input\*.pdf) do (
-::This is the initial conversion of PDF to ePub, but it makes a fugly TOC at the end of the file. So...  the other steps remove that,
-	calibre\ebook-convert "%%f" "calibre\temp\%%~nf.epub" --output-profile tablet --no-default-epub-cover --no-svg-cover
-
-::Decompresses the newly created ePub
-	for %%E in (calibre\temp\*.epub) do (7z.exe x "%%E" -aoa -o"%cd%\calibre\temp\%%~nE")
-
-::Removes the 2 files that hold the TOC
-	del /s /q index_split_001.html
-	del /s /q toc.ncx
-
-::Compresses the folder into a ePub thats wont pass checks
-	for /d %%X in (calibre\temp\*) do (	
-		del /S /Q "%%~nX.epub"
-		pushd "%%X"
-		"%cd%\7z.exe" a "%cd%\calibre\temp\%%~nX.epub" "*" -tzip -mx5
-		popd
-		rmdir  /S /Q "%%X"
-	)
-
-::Converts the ePub so it passes checks via calibre
-	for %%G in (calibre\temp\*.epub) do (	
-		calibre\ebook-convert "%%G" "03-converted\%%~nG.epub" --output-profile tablet --no-default-epub-cover --no-svg-cover
-		del /F "%%G"
-	)
-	
-::Empties the temp dir's (don't delete the directory in the loop!)
-	del /s /q calibre\temp\*.* >NUL
-	
+			if not exist "04-shrunk" mkdir 04-shrunk
+			calibre\ebook-convert "%%f" "calibre\temp\%%~nf.mobi" --output-profile tablet --mobi-keep-original-images --dont-compress 
 )
-::Deletes the temp dir's
-	rmdir  /S /Q "%cd%\calibre\temp"
+		
+for %%b in (calibre\temp\*.mobi) do (	
+			if not exist "04-shrunk" mkdir 04-shrunk
+			calibre\ebook-convert "%%b" "03-converted\%%~nb.epub" --output-profile tablet --remove-first-image --no-svg-cover --no-default-epub-cover 
+)
+	rmdir /S /Q "%cd%\calibre\temp"
